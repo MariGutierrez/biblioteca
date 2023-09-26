7644d9950272e9467391f9752721942012f03602
robttue <robttue@gmail.com>
2023-09-26 14:00:21 -0600

    Modelo de universidad listo

--- WEB/Biblioteca_model/src/com/utl/bli/model/Universidad.java
+++ WEB/Biblioteca_model/src/com/utl/bli/model/Universidad.java
@@ -1,13 +1,59 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.utl.bli.model;
 
-/**
- *
- * @author Gali
- */
+
 public class Universidad {
+    private int id_universidad;
+    private String nombre_universidad;
+    private String pais;
+    private int estatus;
+
+    public Universidad() {
+    }
+
+    public Universidad(int id_universidad, String nombre_universidad, String pais, char estatus) {
+        this.id_universidad = id_universidad;
+        this.nombre_universidad = nombre_universidad;
+        this.pais = pais;
+        this.estatus = estatus;
+    }
+    
+    public int getId_universidad() {
+        return id_universidad;
+    }
+
+    public void setId_universidad(int id_universidad) {
+        this.id_universidad = id_universidad;
+    }
+
+    public String getNombre_universidad() {
+        return nombre_universidad;
+    }
+
+    public void setNombre_universidad(String nombre_universidad) {
+        this.nombre_universidad = nombre_universidad;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public int getEstatus() {
+        return estatus;
+    }
+
+    public void setEstatus(int estatus) {
+        this.estatus = estatus;
+    }
+
+    @Override
+    public String toString() {
+        return "Universidad{" + "id_universidad=" + id_universidad + ", nombre_universidad=" + nombre_universidad + ", pais=" + pais + ", estatus=" + estatus + '}';
+    }
+    
     
 }
