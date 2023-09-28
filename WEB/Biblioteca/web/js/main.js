1a63811a1645450a5a8d6be4f1c9ee143e836572
MariGutierrez <malymiku[@gmail.com>
2023-09-28 09:13:08 -0600

    main

--- WEB/Biblioteca/web/js/main.js
+++ WEB/Biblioteca/web/js/main.js
@@ -1,26 +1,3 @@
-
-
-function ingresar() {
-
-    let usuario = document.getElementById("usuario").value;
-    let contrasenia = document.getElementById("contrasena").value;
-
-    let parametros = new URLSearchParams({usuario: usuario, contrasenia: contrasenia});
-    console.log(parametros);
-    
-
-        fetch("api/log/in",
-                {
-                    method: 'POST',
-                    headers: {'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'},
-                    body: parametros
-
-                }).then(response => response.json())
-                .then(data => {
-//                console.log(data);
-                   window.location.href = 'inicio.html';
-                });
-}
 let moduloUniversidad;
 
 function cargarModuloUniversidad(){
@@ -37,6 +14,28 @@ function cargarModuloUniversidad(){
                     function(controller){
                         moduloUniversidad = controller;
                         moduloUniversidad.inicializar();
+                    }
+                );
+            }
+        );
+}
+
+let moduloLibro;
+
+function cargarmoduloLibro(){
+    fetch("modulo_libro/view_libro.html")
+        .then(
+            function(response){
+                return response.text();
+            }
+        )
+        .then(
+            function(html){
+                document.getElementById("contPrincipal").innerHTML = html;
+                import ("modulo_libro/controller_libro.js").then(
+                    function(controller){
+                        moduloLibro = controller;
+                        moduloLibro.inicializar();
                     }
                 );
             }
